3c8249c5f5c98bfa103fb9f2b98aeb44d4ced77d
ParamvirSingh96 <paramvirs1996@gmail.com>
2025-06-24 16:09:37 -0400

    Removed FullStack folder and package-lock.json

--- FullStack/frontend/src/components/LoadingIndicator.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/LoadingIndicator.css"
-
-const LoadingIndicator = () => {
-    return <div className="loading-cpntainer">
-        <div classname="loader"></div>
-    </div>
-}
-
-export default LoadingIndicator
